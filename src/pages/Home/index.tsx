8cc232e60b59fe7c7684d8c8646090f7406834f7
Maíse <esiammd@hotmail.com>
2020-08-21 02:16:36 -0300

    Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,16 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { FormEvent, useState } from "react";
 
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const [cpf, setCpf] = useState("");
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+  }
+
   return (
     <div id="page-home">
       <header>
         <Link to="/login">Login</Link>
       </header>
 
-      <h1>Home</h1>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <h1>Home</h1>
+
+          <div id="field-input">
+            <label>CPF:</label>
+            <input
+              type="text"
+              value={cpf}
+              onChange={(event) => setCpf(event.target.value)}
+            />
+          </div>
+
+          <button type="submit" id="button">
+            Enviar
+          </button>
+        </form>
+      </main>
     </div>
   );
 }
