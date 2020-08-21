6ee1921285f5cf656545704a65c4a54ed570adf0
Maíse <esiammd@hotmail.com>
2020-08-21 02:15:24 -0300

    Logout

--- src/pages/Admin/index.tsx
+++ src/pages/Admin/index.tsx
@@ -1,13 +1,59 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+// import api from "../../services/api";
 
 import "./styles.css";
 
-const Admin = () => {
+function Admin() {
+  const history = useHistory();
+
+  const [file, setFile] = useState("");
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+
+    history.push("/");
+  }
+
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+
+    try {
+      const token = localStorage.getItem("token");
+      console.log("token: ", token);
+    } catch (erro) {
+      alert("Erro");
+    }
+  }
+
   return (
     <div id="page-admin">
-      <h1>Admin</h1>
+      <header>
+        <button type="button" onClick={handleLogout}>
+          Logout
+        </button>
+      </header>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <h1>Página do Administrador</h1>
+
+          <div id="field-input">
+            <label>Arquivo:</label>
+            <input
+              type="text"
+              value={file}
+              onChange={(event) => setFile(event.target.value)}
+            />
+          </div>
+
+          <button type="submit" id="button">
+            Enviar
+          </button>
+        </form>
+      </main>
     </div>
   );
-};
+}
 
 export default Admin;
--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 
 import "./styles.css";
 
-const Page404 = () => {
+function Page404() {
   return (
     <div id="page-page404">
       <h1>404</h1>
       <span>Página não encontrada</span>
     </div>
   );
-};
+}
 
 export default Page404;
