bf9fa572a7722f705cd22cc6f854de3949665150
Maíse <esiammd@hotmail.com>
2020-08-31 00:50:09 -0300

    Page 404

--- src/pages/Page404/index.tsx
+++ src/pages/Page404/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 function Page404() {
   return (
     <div id="page-page404">
-      <h1>404</h1>
-      <span>Página não encontrada</span>
+      <h1>Opsss!</h1>
+      <span>ERRO 404 - PÁGINA NÃO ENCONTRADA</span>
+      <Link to="/">Ir para Home</Link>
     </div>
   );
 }
